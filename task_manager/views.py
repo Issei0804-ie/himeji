47bb5754d8c37f4e2c99a9c5e07dd22d34c24968
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-01-14 12:19:11 +0900

    ユーザー登録機能の追加

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.template import loader
-
-#from .models import Question
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 def index(request):
-    context = {
-        #'latest_question_list': latest_question_list,
-    }
+    context = {}
     template = loader.get_template('task_manager/login.html')
     
     return HttpResponse(template.render(context, request))
+
+def register(request):
+    request_username = request.POST['username']
+    request_password = request.POST['password']
+
+    try:
+        User.objects.get(username=request_username)
+    except User.DoesNotExist:
+        user = User.objects.create_user(request_username, '', request_password)
+        user.save()
+        result = authenticate(username=request_username, password=request_password)
+        if result is not None:
+            return HttpResponseRedirect('task_manager')
+
+    return HttpResponseBadRequest()
+
